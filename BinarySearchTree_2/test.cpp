b9e12acf7cdcfda826fdf07fb6b94e03edaf6a5e
YuFan-Ji <972321520@qq.com>
2024-10-31 12:29:02 +0800

    BinarySearchTree_2 二叉搜索树进阶，递归版本，K模型与K_value模型 2024/10/31

---
+++ BinarySearchTree_2/test.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+#include"BSTree.h"
+
+//int main()
+//{
+//	key::BSTree<int> t;
+//	//t.TestBSTree1();
+//	t.TestBSTree2();
+//	return 0;
+//
+//	key_value::TestBSTree2();
+//}
+
+int main()
+{
+	key_value::TestBSTree2();
+
+	return 0;
+}
